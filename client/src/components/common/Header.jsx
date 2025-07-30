86bd5d901b621426f9fb1859893b268fbf9f0400
kanthTs <tsiddakanth@gmail.com>
2025-07-30 19:17:31 +0530

    messages

---
+++ client/src/components/common/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import image from '/Users/lakshmikanth/interview-project/client/images/interview-job-logo-design-vector-260nw-2015326151.webp'
+import { Link } from 'react-router-dom'
+function Header() {
+  return (
+    <div style={{backgroundColor:"ButtonFace",padding:"10px",display:"flex",justifyContent:"space-between"}}>
+      <div >
+        <img src={image} style={{width:"80px",borderRadius:"50%"}}/>
+      </div>
+    <div style={{display:"flex",justifyContent:"space-between",gap:"60px",margin:"20px"}}>
+     <Link to="/signin" style={{color:"black",padding:"10px",}}>SignIn</Link>
+     <Link to="/signup" style={{color:"black",padding:"10px"}}>SignUp</Link>
+    </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/components/common/Signup.jsx
@@ -0,0 +1,12 @@
+import { SignUp } from '@clerk/clerk-react'
+import React from 'react'
+
+function Signup() {
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      <SignUp/>
+    </div>
+  )
+}
+
+export default Signup
