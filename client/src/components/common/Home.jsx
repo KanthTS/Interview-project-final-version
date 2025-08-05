4a92ce89ecfee905e60262a3c86632eccc35b0f6
Zakir <zakirwemail123@gmail.com>
2025-08-05 18:34:52 +0530

    removed node_modules

--- client/src/components/common/Home.jsx
+++ client/src/components/common/Home.jsx
@@ -1,36 +1,19 @@
-<<<<<<< HEAD
-
-=======
-import React from 'react'
-import "./Home.css"
->>>>>>> adb3c0ab3cd8aef6910b98466b27366b9ccf7405
 import { Link } from 'react-router-dom'
-
+import './Home.css'
 function Home() {
 
   return (
 
     <div>
      
-<<<<<<< HEAD
-      <div className="p-2">
-         <Link to="dsa">DSA</Link>
-          <Link to="backend">Backend</Link>
-          <Link to="frontend">Frontend</Link>
-          <Link to="database">Data Bases</Link>
-          <Link to="devops">DevOps</Link>   
-           <Link to="corecs">Core CS</Link>  
-           <Link to="software">SoftWare engneering</Link>   
-=======
-      <div className="p-2 " style={{display:'flex',justifyContent:'space-between',borderRadius:'15px'}}>
+       <div className="p-2 " style={{display:'flex',justifyContent:'space-between',borderRadius:'15px'}}>
          <Link to=""  className='custom-link' style={{textDecoration:'none'}}>DSA</Link>
           <Link to="" className='custom-link' style={{textDecoration:'none'}}>Backend</Link>
           <Link to="" className='custom-link' style={{textDecoration:'none'}}>Frontend</Link>
           <Link to="" className='custom-link' style={{textDecoration:'none'}}>Data Bases</Link>
           <Link to="" className='custom-link' style={{textDecoration:'none'}}>DevOps</Link>   
            <Link to="" className='custom-link' style={{textDecoration:'none'}}>Core CS</Link>  
-           <Link to="" className='custom-link' style={{textDecoration:'none'}}>SoftWare engneering</Link>   
->>>>>>> adb3c0ab3cd8aef6910b98466b27366b9ccf7405
+           <Link to="" className='custom-link' style={{textDecoration:'none'}}>SoftWare engneering</Link>  
 
    
       </div>
@@ -41,3 +24,7 @@ function Home() {
 }
 
 export default Home
+
+
+
+
